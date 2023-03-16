fa79f69ca0574837c81a8a56cdbc872477e448be
Adnan <adnan.silajdzic.it@gmail.com>
2023-03-16 09:51:42 +0100

    first commit

---
+++ src/components/helloworld.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './css/helloworld.css'
+const helloworld = () => {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  )
+}
+
+export default helloworld
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Helloworld from './components/helloworld';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Helloworld />
+);
+
