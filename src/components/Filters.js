3da66a3f74390b7a505229263293c361dc2e2f3b
Adnan <adnan.silajdzic.it@gmail.com>
2023-03-18 15:34:38 +0100

    filters can be added and removed

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,33 +1,47 @@
-import React from "react";
-
+import React, { useState } from "react";
 import close from "../images/icons/close.png";
+
 const Filters = () => {
+  const [filters, setFilters] = useState(["pastry", "dessert", "chocolate"]);
+  const [newFilter, setNewFilter] = useState("");
+
+  const handleAddFilter = (event) => {
+    event.preventDefault();
+    setFilters([...filters, newFilter]);
+    setNewFilter("");
+  };
+
+  const handleRemoveFilter = (filterToRemove) => {
+    setFilters(filters.filter((filter) => filter !== filterToRemove));
+  };
+
   return (
     <div>
       {/*Filters*/}
 
       <div className="filters">
         <p>Filters:</p>
-        <div className="filter">
-          <p>
-            pastry and hoes <img src={close}></img>
-          </p>
-        </div>
-
-        <div className="filter">
-          <p>
-            dessert <img src={close}></img>
-          </p>
-        </div>
-
-        <div className="filter">
-          <p>
-            chocolate <img src={close}></img>
-          </p>
-        </div>
+        {filters.map((filter, index) => (
+          <div key={index} className="filter">
+            <p>
+              {filter}
+              <img
+                src={close}
+                alt="Remove Filter"
+                onClick={() => handleRemoveFilter(filter)}
+              />
+            </p>
+          </div>
+        ))}
         <div className="line"></div>
         <div className="addFilter">
-          <input placeholder="Add filter"></input>
+          <form onSubmit={handleAddFilter}>
+            <input
+              placeholder="Add filter"
+              value={newFilter}
+              onChange={(event) => setNewFilter(event.target.value)}
+            />
+          </form>
         </div>
       </div>
     </div>
