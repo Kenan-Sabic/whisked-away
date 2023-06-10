a62308441e258efaf12d1e492336943f3c8ee0df
Ismar1509 <ismaraganovic69@gmail.com>
2023-06-10 16:02:57 +0200

    Newsletter box, download pdf of recipe of the day

--- src/components/Daily_recipe.jsx
+++ src/components/Daily_recipe.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import { jsPDF } from "jspdf";
+import "jspdf-autotable";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -15,7 +17,7 @@ import krosan3 from "../assets/krosan3.jpg";
 import SliderPlaceholder from "./SliderPlaceholder";
 
 const Daily_Recipe = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = React.useState(false);
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -29,23 +31,44 @@ const Daily_Recipe = () => {
     setShowModal(false);
   };
 
-  
   const handleClick = () => {
-		if (!token) {
-			window.location.href = '/login';
-		} else {
-			window.location.href = '/user/recipes/add';
-		}
-	};
-	const token = sessionStorage.getItem('token'); 
+    generatePDF();
+  };
+
+  const recipeTitle = "Croissants";
+  const recipeServes = "4";
+  const recipePrepTime = "15-20 minutes";
+  const recipeCookTime = "2 hours";
+  const recipeIngredients = "500g all-purpose flour\n10g salt \n80g granulated sugar\n25g fresh yeast or 10g active dry yeast\n300ml warm milk\n250g unsalted butter, chilled\n1 egg, beaten";
+  const recipeInstructions ='In a large mixing bowl, combine the flour, salt, and sugar.\n If using fresh yeast, dissolve it in the warm milk.\n If using active dry yeast, dissolve it in 1/4 cup warm water with a pinch of sugar, and let it sit for 5 minutes.\n Add the yeast mixture to the flour mixture, and stir until a shaggy dough forms.\n Turn the dough out onto a lightly floured surface, and knead for 10 minutes until smooth and elastic. \nShape the dough into a ball, and place it in a lightly oiled bowl. \n Cover the bowl with plastic wrap, and let the dough rise in a warm place for 1 hour, until doubled in size. \nAfter the dough has risen, punch it down and shape it into a rectangle.\n Cut the chilled butter into thin slices, and place them in a single layer over two-thirds of the dough. \nFold the uncovered third of the dough over the butter, then fold the other third over the top, like a letter.\n Turn the dough 90 degrees, and roll it out into a long rectangle again.\nFold the rectangle into thirds again, like a letter.\nWrap the dough in plastic wrap, and refrigerate for at least 1 hour, or overnight.\nAfter the dough has chilled, roll it out into a long rectangle, about 1/4 inch thick.\nCut the rectangle into triangles, and roll each triangle up from the wide end to the narrow end, tucking the narrow end underneath.\nPlace the croissants on a baking sheet lined with parchment paper, and brush them with beaten egg.\nLet the croissants rise in a warm place for 30-45 minutes, until they are puffy and doubled in size.\nPreheat the oven to 400°F (200°C).\nBrush the croissants with egg again, and bake for 15-20 minutes, until golden brown.\nRemove the croissants from the oven, and let them cool on a wire rack before serving.\n Enjoy!';
+
+  const generatePDF = () => {
+    const doc = new jsPDF();
+
+    doc.setFontSize(16);
+    doc.text(`Recipe of the Day - ${recipeTitle}`, 10, 10);
+
+    const tableData = [
+      ["Serves", recipeServes],
+      ["Prep Time", recipePrepTime],
+      ["Cook Time", recipeCookTime],
+      ["Ingredients", recipeIngredients],
+      ["Instructions", recipeInstructions],
+    ];
+
+    doc.autoTable({
+      startY: 30,
+      head: [["Property", "Value"]],
+      body: tableData,
+    });
+
+    doc.save("recipe.pdf");
+  };
 
   return (
     <div className="flex flex-col items-center">
-      {/*Recipe of the Day*/}
-
-      <div className="daily-recipes mx-0 my-auto flex flex-wrap justify-center">
-        {" "}
-      </div>
+      {/* Recipe of the Day */}
+      <div className="daily-recipes mx-0 my-auto flex flex-wrap justify-center"></div>
       <div className="mt-10 w-3/4 overflow-hidden rounded-lg border border-neutral-600 bg-neutral-600">
         <div className="border-b-4 border-sandybrown bg-neutral-600 px-8 py-4">
           <h2 className="text-center text-2xl font-extrabold text-white md:text-left">
@@ -62,36 +85,12 @@ const Daily_Recipe = () => {
       </div>
       {showModal && (
         <DailyRecipeModal
-          title="Croissants"
-          serves="4"
-          prepTime="15-20 minutes"
-          cookTime="2 hours"
-          ingredients=" 
-        - 500g all-purpose flour
-        - 10g salt
-        - 80g granulated sugar
-        - 25g fresh yeast or 10g active dry yeast
-        - 300ml warm milk
-        - 250g unsalted butter, chilled
-        - 1 egg, beaten"
-          instructions="In a large mixing bowl, combine the flour, salt, and sugar.
-        If using fresh yeast, dissolve it in the warm milk. If using active dry yeast, dissolve it in 1/4 cup warm water with a pinch of sugar, and let it sit for 5 minutes.
-        Add the yeast mixture to the flour mixture, and stir until a shaggy dough forms.
-        Turn the dough out onto a lightly floured surface, and knead for 10 minutes until smooth and elastic.
-        Shape the dough into a ball, and place it in a lightly oiled bowl. Cover the bowl with plastic wrap, and let the dough rise in a warm place for 1 hour, until doubled in size.
-        After the dough has risen, punch it down and shape it into a rectangle.
-        Cut the chilled butter into thin slices, and place them in a single layer over two-thirds of the dough.
-        Fold the uncovered third of the dough over the butter, then fold the other third over the top, like a letter.
-        Turn the dough 90 degrees, and roll it out into a long rectangle again.
-        Fold the rectangle into thirds again, like a letter.
-        Wrap the dough in plastic wrap, and refrigerate for at least 1 hour, or overnight.
-        After the dough has chilled, roll it out into a long rectangle, about 1/4 inch thick.
-        Cut the rectangle into triangles, and roll each triangle up from the wide end to the narrow end, tucking the narrow end underneath.
-        Place the croissants on a baking sheet lined with parchment paper, and brush them with beaten egg.
-        Let the croissants rise in a warm place for 30-45 minutes, until they are puffy and doubled in size.
-        Preheat the oven to 400°F (200°C).
-        Brush the croissants with egg again, and bake for 15-20 minutes, until golden brown.
-        Remove the croissants from the oven, and let them cool on a wire rack before serving. Enjoy!"
+          title={recipeTitle}
+          serves={recipeServes}
+          prepTime={recipePrepTime}
+          cookTime={recipeCookTime}
+          ingredients={recipeIngredients}
+          instructions={recipeInstructions}
           imageSrc={krosan1}
           handleCloseModal={handleCloseModal}
           onHide={handleHideModal}
@@ -99,16 +98,15 @@ const Daily_Recipe = () => {
       )}
 
       <div>
-      <button
-      onClick={handleClick}
-      className="fixed bottom-2 right-2 shadow-lg p-4 bg-sandybrown text-white rounded-full z-50"
-    >
-      {!token ? 'Add your recipe' : 'Add your recipe'}
-    </button>
+        <button
+          onClick={handleClick}
+          className="fixed bottom-2 right-2 shadow-lg p-4 bg-sandybrown text-white rounded-full z-50"
+        >
+          Download Recipe PDF
+        </button>
       </div>
     </div>
   );
-}
-;
+};
 
 export default Daily_Recipe;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,56 +3,68 @@ import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 function Footer() {
   return (
-   <>
-    <div className="bg-sandybrown h-1"></div>
-    <footer className="bg-neutral-700 py-8 lg:py-12">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-wrap justify-between">
-          <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
-            <h3 className="text-lg font-semibold text-gray-100 mb-4">About Us</h3>
-            <p className="text-gray-400 leading-loose">
-               Faris Hadziabdic <br/>
-               Adnan Silajdzic <br/>
-               Kenan Sabic <br />
-               Ismar Aganovic <br />
-               Nedzmija Karic <br />
-            </p>
+    <>
+      <div className="bg-sandybrown h-1"></div>
+      <footer className="bg-neutral-700 py-8 lg:py-12">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-wrap justify-between">
+            <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
+              <h3 className="text-lg font-semibold text-gray-100 mb-4">About Us</h3>
+              <p className="text-gray-400 leading-loose">
+                Faris Hadziabdic <br/>
+                Adnan Silajdzic <br/>
+                Kenan Sabic <br />
+                Ismar Aganovic <br />
+                Nedzmija Karic <br />
+              </p>
+            </div>
+            <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
+              <h3 className="text-lg font-semibold text-gray-100 mb-4">Navigation</h3>
+              <ul className="text-gray-400 leading-loose">
+                <li><a href="/">Home</a></li>
+                <li><a href="/browse">Browse</a></li>
+                <li><a href="/profile">Profile</a></li>
+              </ul>
+            </div>
+            <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
+              <h3 className="text-lg font-semibold text-gray-100 mb-4">Newsletter</h3>
+              <ul className="text-gray-400 leading-loose">
+              <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                />
+              </ul>
+              <div className="mt-4">
+                
+                <button
+                  type="button"
+                  className="bg-sandybrown text-white px-4 py-2 rounded-md hover:bg-orange-400 focus:outline-none focus:bg-orange-400"
+                >
+                  Subscribe
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
-            <h3 className="text-lg font-semibold text-gray-100 mb-4">Navigation</h3>
-            <ul className="text-gray-400 leading-loose">
-              <li><a href="/">Home</a></li>
-              <li><a href="/browse">Browse</a></li>
-              <li><a href="/profile">Profile</a></li>
-            </ul>
-          </div>
-          <div className="w-full lg:w-1/3 mb-8 lg:mb-0">
-            <h3 className="text-lg font-semibold text-gray-100 mb-4">Newsletter</h3>
-            <ul className="text-gray-400 leading-loose">
-              <li>Link 1</li>
-              <li>Link 2</li>
-              <li>Link 3</li>
-            </ul>
+          <div className="flex justify-between items-center mt-8">
+            <div className="flex items-center">
+              <a href="#" className="text-orange-500 hover:text-orange-400 mr-4">
+                <FaFacebook />
+              </a>
+              <a href="#" className="text-orange-500 hover:text-orange-400 mr-4">
+                <FaTwitter />
+              </a>
+              <a href="#" className="text-orange-500 hover:text-orange-400">
+                <FaInstagram />
+              </a>
+            </div>
+            <p className="text-gray-400 text-sm">&copy; 2023. All rights reserved.</p>
           </div>
         </div>
-        <div className="flex justify-between items-center mt-8">
-          <div className="flex items-center">
-            <a href="#" className="text-orange-500 hover:text-orange-400 mr-4">
-              <FaFacebook />
-            </a>
-            <a href="#" className="text-orange-500 hover:text-orange-400 mr-4">
-              <FaTwitter />
-            </a>
-            <a href="#" className="text-orange-500 hover:text-orange-400">
-              <FaInstagram />
-            </a>
-          </div>
-          <p className="text-gray-400 text-sm">&copy; 2023. All rights reserved.</p>
-        </div>
-      </div>
-    </footer>
+      </footer>
     </>
   );
 }
 
 export default Footer;
+
